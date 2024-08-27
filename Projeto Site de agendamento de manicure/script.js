c05e3b0feeeed8913683bc2b476c4b3e25be4dbb
lilPaulo <44075919+lilPaulo@users.noreply.github.com>
2024-08-26 22:24:35 -0300

    Add files via upload

---
+++ Projeto Site de agendamento de manicure/script.js
@@ -0,0 +1,20 @@
+/*alert("Olá, Hello Word !!") Primeiro codigo em JavaScript!! Agora quero ver o php do homi kkkkk*/
+
+/*variaveis
+var idade = "35"
+var nome = "maria"*/
+/*caixa de texto tipo conole.log
+console.log ('olá, ' + nome + ' eu fiquei sabendo que você tem ' + idade + ' anos de idade.')*/
+
+/*alert cria uma caixa de texto no topo da pagina executavel e com o tempo posso usala para passar uma mensagem de erro.(If e else)
+alert ('olá, ' + nome + ' eu fiquei sabendo que você tem ' + idade + ' anos de idade.')
+*/
+
+/* Desafio de logica
+var a = 10
+var b = 20
+var c = null*/
+
+/*document.write ('a: ' + a + )
+document.write ('b: ' + a + )
+document.write ('c: ' + a + )*/
